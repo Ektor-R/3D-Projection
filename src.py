c1e2a643c8ca26bcfc89f931a1212c96e93623b1
Ektoras Rontos <rontekto@gmail.com>
2022-05-13 02:46:07 +0300

    Affine Transformation

--- src.py
+++ src.py
@@ -8,19 +8,54 @@ CAMERA_HEIGHT = 15
 F = 70
 BACKGROUND = [1., 1., 1.]
 
-def affine_transform(point: np.array, angle: float, axis: np.array, displacement: np.array) -> np.array:
+
+
+def affine_transform(point: np.ndarray, angle = 0., axis: np.ndarray = np.nan, translation = np.array([0, 0, 0])) -> np.ndarray:
     """
         Perform 3D Affine Transformation
 
         Arguments:
-            point: Point to transform.
+            point: Point(s) to transform.
             angle: Angle of rotation.
             axis: Axis of rotation
-            displacement: Displacement of point
+            translation: Translation of point(s)
 
         Returns:
-            new point
+            new point(s) coordinates
     """
+
+    # Pass rotation if axis is not defined
+    if axis is not np.nan:
+        # Get unit vector of axis
+        u = axis / np.linalg.norm(axis)
+
+        sinA = np.sin(angle)
+        cosA = np.cos(angle)
+
+        rotationMatrix = np.array([
+            [
+                (u[0]**2) * (1 - cosA) + cosA,
+                u[0] * u[1] * (1 - cosA) - u[2] * sinA,
+                u[0] * u[2] * (1 - cosA) + u[1] * sinA
+            ],
+            [
+                u[1] * u[0] * (1 - cosA) + u[2] * sinA,
+                (u[1]**2) * (1 - cosA) + cosA,
+                u[1] * u[2] * (1 - cosA) - u[0] * sinA
+            ],
+            [
+                u[2] * u[0] * (1 - cosA) - u[1] * sinA,
+                u[2] * u[1] * (1 - cosA) + u[0] * sinA,
+                (u[2]**2) * (1 - cosA) + cosA
+            ]
+        ])
+
+        # Rotate
+        # Transpose coordinates before and after rotation in case multiple points have been given
+        point = np.matmul(rotationMatrix, point.transpose()).transpose()
+
+    # Translate
+    point = np.add(point, translation)
 
     return point
 
